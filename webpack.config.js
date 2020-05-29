f0a75ef99bb0f6ad7cbeae6bc50c37ed5c73bc89
Jorge Barbosa <jorgeluis6@gmail.com>
2020-05-29 15:05:10 -0700

    setup express

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
-
 const path = require('path')
 
+module.exports = (env) => {
 
-module.exports = {
-    entry: './src/app.js',
+    const isProduction = env === 'production';
+
+    return {
+        entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
@@ -23,8 +25,9 @@ module.exports = {
             ]
         }]
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public')
+    }
     }
 }
